90097a2cbf3a68fa496dc465c0c40a04f2f821f4
TrevorHarron <trharron@gmail.com>
2014-10-05 16:58:38 -0700

    added the base function to build the others on

--- app/controllers/Math.scala
+++ app/controllers/Math.scala
@@ -8,34 +8,35 @@ object Math extends Controller {
     
   private val defaultMSG = 
     NotImplemented(Json.obj("message"->"sorry this is not ready", "Status"->501))
-  private def mapReduce = TODO
   
-  def home = Action{
-      defaultMSG
-  }
+  
+  private def baseFunction(base:Double, values: Array[Double],
+    func: (Double,Double) => Double, cond: Double => Boolean):Double = {
+        def iter(acc:Double,values:Array[Double], 
+            f: (Double,Double) => Double, c: Double => Boolean):Double = {
+            if(values.isEmpty){ 
+                base
+            } else if (c(values.head)) {
+                throw new IllegalArgumentException(values.head + " was invalid")
+            } else { 
+                iter(f(acc,values.head), values.tail,f,c)
+            }
+        }
+        iter(base, values,func,cond)
+    }
+  
+  def home = TODO
 
-  def add = Action{
-      defaultMSG
-  }
+  def add = Action(parse.json){request => defaultMSG }
   
-  def sub = Action{
-      defaultMSG
-  }
+  def sub = Action(parse.json){request => defaultMSG }
   
-  def mul = Action{
-      defaultMSG
-  }
+  def mul = Action(parse.json){request => defaultMSG }
   
-  def div = Action{
-      defaultMSG
-  }
+  def div = Action(parse.json){request => defaultMSG }
   
-  def mod = Action{
-      defaultMSG
-  }
+  def mod = Action(parse.json){request => defaultMSG }
   
-  def pwr = Action{
-      defaultMSG
-  }
+  def pwr = Action(parse.json){request => defaultMSG }
 
 }
