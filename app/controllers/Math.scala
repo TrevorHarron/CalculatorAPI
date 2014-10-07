19d81174f57fe302b9219e8556e9f0a3ff3a0e2d
TrevorHarron <trharron@gmail.com>
2014-10-06 23:38:06 -0700

    refactored add for generic fuctions

--- app/controllers/Math.scala
+++ app/controllers/Math.scala
@@ -10,41 +10,45 @@ object Math extends Controller {
     
   implicit val rds = ((__ \ 'values).read[List[Double]])
   
-  def add = Action{ request => 
-       request.body.asJson.map{ json =>
-       Logger.debug(json.getClass.toString)
-           json.validate[(List[Double])].map{
-                case (values) => {
-                   var result = 0.0
-                   try {
-                       var result = baseFunction(0, values, (x:Double,y:Double)=>x+y, (x:Double)=>false)
-                       Ok(Json.obj("result"-> result))
-                    } catch {
-                       case ex: IllegalArgumentException => {BadRequest(Json.obj("message"->"Illegal arguments", "Status"->400))}
-                       case ex: Exception => {InternalServerError(Json.obj("message"->"Unknown Error", "Status"->500))}
-                    }
-                    }
-                }.recoverTotal {
-                    ex => BadRequest(JsError.toFlatJson(ex))
+  def handleJson(json:JsObject,start:Double, values:List[Double],f:(Double,Double)=>Double,c: Double => Boolean) ={
+        json.validate[(List[Double])].map{
+            case (values) => {
+                var result = 0.0
+                try {
+                    var result = baseFunction(start, values, f, c)
+                    Ok(Json.obj("result"-> result))
+                } catch {
+                   case ex: IllegalArgumentException => {BadRequest(Json.obj("message"->"Illegal arguments", "Status"->400))}
+                   case ex: Exception => {InternalServerError(Json.obj("message"->"Unknown Error", "Status"->500))}
                 }
+            }
+        }.recoverTotal {
+            ex => BadRequest(JsError.toFlatJson(ex))
+        }
+  }
+  
+  
+    def add = Action{ request => 
+             request.body.asJson.map{ json =>
+                handleJson(json,0.0,values,(d:Double,y:Double)=>x+y,(x:Double)=>false)
             }.getOrElse {
               BadRequest("Expecting Json data")
             }
-        }
+    }
   
-  def sub = Action(parse.json){request => defaultMSG }
+    def sub = Action(parse.json){request => defaultMSG }
   
-  def mul = Action(parse.json){request => defaultMSG }
+    def mul = Action(parse.json){request => defaultMSG }
   
-  def div = Action(parse.json){request => defaultMSG }
+    def div = Action(parse.json){request => defaultMSG }
   
-  def mod = Action(parse.json){request => defaultMSG }
+    def mod = Action(parse.json){request => defaultMSG }
   
-  def pwr = Action(parse.json){request => defaultMSG }
+    def pwr = Action(parse.json){request => defaultMSG }
   
-  def home = Action{request => defaultMSG }
+    def home = Action{request => defaultMSG }
   
-  private val defaultMSG = NotImplemented(Json.obj("message"->"sorry this is not ready", "Status"->501))
+    private val defaultMSG = NotImplemented(Json.obj("message"->"sorry this is not ready", "Status"->501))
   
   private def baseFunction(base:Double, values: List[Double],
     func: (Double,Double) => Double, cond: Double => Boolean):Double = {
